dbb82859fe4228ac4e140371e90792833d65b81f
RepellantHail <a20310068@ceti.mx>
2023-11-22 10:47:59 -0600

    Count words

--- app/Results.js
+++ app/Results.js
@@ -3,6 +3,24 @@ import React, { useState } from "react";
 import ResultRow from "./ResultRow";
 
 export default function Result({ title, content }) {
+    const palabrasReservadas = [
+        "if",
+        "else",
+        "switch",
+        "case",
+        "default",
+        "for",
+        "while",
+        "break",
+        "int",
+        "String",
+        "double",
+        "char",
+        "print",
+    ];
+
+    const whitespaceChars = ["\n", "\t", " "];
+
     const initialData = [
         { label: "Palabras reservadas", value: 0 },
         { label: "Identificadores", value: 0 },
@@ -26,7 +44,46 @@ export default function Result({ title, content }) {
         window.location.reload();
     };
 
-    const processData = () => {};
+    const processData = () => {
+        const charText = content.split(""); //Convert content to char Array
+        let word = "";
+
+        charText.forEach((c) => {
+            //Traverse the content
+            if (whitespaceChars.includes(c)) {
+                processWord(word);
+                word = "";
+            } else {
+                word += c;
+            }
+        });
+
+        // Update the state after the loop
+        setProcessedData((prevData) => {
+            const updatedData = [...prevData];
+            return updatedData;
+        });
+    };
+
+    const processWord = (word) => {
+        setProcessedData((prevData) => {
+            const updatedData = prevData.map((item) => {
+                switch (item.label) {
+                    case "Palabras reservadas":
+                        if (palabrasReservadas.includes(word.toLowerCase())) {
+                            return { ...item, value: item.value + 1 };
+                        }
+                        break;
+
+                    default:
+                        return item;
+                }
+                return item;
+            });
+
+            return updatedData;
+        });
+    };
 
     return (
         <div className="col-4 ">
